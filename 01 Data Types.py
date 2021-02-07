5238683a2be6d4b390f0092b23f396b27bf68202
afrahaamer <30327502+afrahaamer@users.noreply.github.com>
2021-02-07 15:25:44 +0530

    ran data types in python

---
+++ 01 Data Types.py
@@ -0,0 +1,16 @@
+# ALGORITHM
+# Step 1: Start
+# Step 2: Enter variable and number
+# Step 3: Read variable with number
+# Step 4: Print variable's value/type
+# Step 5: Stop
+
+# CODE
+a = 5
+print(a,"is of type",type(a))
+
+a = 2.0
+print(a,"is of type",type(a))
+
+a = 1+2j
+print(a,"is of type",type(a))
