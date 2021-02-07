aaa2526b28bab677eea527622b848b308e24d553
afrahaamer <30327502+afrahaamer@users.noreply.github.com>
2021-02-07 23:27:10 +0530

    Executed Right Triangle program

---
+++ 13 Right Triangle.py
@@ -0,0 +1,7 @@
+a = int(input("Length of side 1:"))
+b = int(input("Length of side 2:"))
+c = int(input("Length of side 3:"))
+if (a^2 + b^2) == c^2:
+    print("It's a right triangle")
+else:
+    print("It's not a right triangle")
