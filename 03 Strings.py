683badcbb2dca26cec52f38fb1a08d7983b709e4
afrahaamer <30327502+afrahaamer@users.noreply.github.com>
2021-02-07 15:53:57 +0530

    Executed 3rd and 4th programs

---
+++ 03 Strings.py
@@ -0,0 +1,10 @@
+# CODE
+s1 = "This is 'Afrah'"
+s2 = "This is "
+s3 = "'Afrah'"
+s4 = s2 + s3
+print(s4)
+print('s1[0]',s1[0])
+print('s1[-1]',s1[-1])
+print('s1[1:5]',s1[1:5])
+print('s1[5:-2]',s1[5:-2])
