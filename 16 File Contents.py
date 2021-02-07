68446523ae94304b24d35def98f69eec24fe0032
afrahaamer <30327502+afrahaamer@users.noreply.github.com>
2021-02-07 23:35:15 +0530

    File Contents program

---
+++ 16 File Contents.py
@@ -0,0 +1,9 @@
+sfilename=input("source filename:")
+dfilename=input("Destination filename:")
+f1=open(sfilename,'r')
+f2=open(dfilename,'w')
+for line in f1.readline():
+    f2.write(line)
+    print("file copied successful")
+f1.close()
+f2.close()
