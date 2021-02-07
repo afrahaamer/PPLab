8ac34d717643c5842c13cb500fce3ab2242ab04f
afrahaamer <30327502+afrahaamer@users.noreply.github.com>
2021-02-07 15:47:11 +0530

    Ran second program

---
+++ 02 Arithmetic Operations.py
@@ -0,0 +1,18 @@
+# CODE
+num1 = int(input('num1 = '))
+num2 = int(input('num2 = '))
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+floor = num1 // num2
+pow = num1 ** num2
+mod = num1 % num2
+print("For values ", num1, "and ", num2, ":\n")
+print("Sum is ", add)
+print("Difference is ", sub)
+print("Product is ", mul)
+print("Quotient is ", div)
+print("Floor Quotient is ", floor)
+print("Exponentiation is ", pow)
+print("Remainder is ", mod)
